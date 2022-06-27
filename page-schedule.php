d35e82a7f84a58afe2284c9c3b5c1578f8ca7ce2
ckodev <cowens17@my.bcit.ca>
2022-06-27 15:10:40 -0700

    output event title, thumbnail and tax-term

--- page-schedule.php
+++ page-schedule.php
@@ -31,20 +31,55 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 
-<section>
-			<h2>Schedule</h2>
+		<section>
 
 			<?php
 
-			if (function_exists('get_field')) {
-				if (get_field('description')) {
+			if (function_exists('get_field')) :
+				if (get_field('description')) :
 					?>
 						<p><?php the_field('description'); ?></p>
 					<?php
+				endif;
+			endif;
+
+			
+
+			
+
+			$args = array(
+				'post_type' 	 => 'fan-event',
+				'posts_per_page' => -1,
+				'orderby'        => 'meta_value',
+    			'meta_key'       => 'start_time',
+				'order' 		 => 'ASC',
+			);
+
+		
+			$query = new WP_Query( $args );
+			if ( $query -> have_posts() ) {
+				
+			
+				if ( $query -> have_posts() ) {
+					echo "<div class='anchor-container'>";
+					while ( $query -> have_posts() ) {
+						$query -> the_post();
+
+						echo "<article class='student-item'>";
+								echo '<a href="'.get_permalink().'">';
+									echo "<h2>".get_the_title()."</h2>";
+									the_post_thumbnail( 'thumbnail' );
+								echo '</a>';
+							the_excerpt();
+							echo "<p>".the_taxonomies()."</p>";
+						echo "</article>";
+		
+					}
+					wp_reset_postdata();
+					echo "</div>";
 				}
+			wp_reset_postdata();
 			}
-			
-			
 			?>
 			
 
