cd28d78687d39d42ab2921d3f2e52ad4cd4f6264
ckodev <cowens17@my.bcit.ca>
2022-06-28 14:54:17 -0700

    delete content from carried over from page schedule copy

--- front-page.php
+++ front-page.php
@@ -21,98 +21,14 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 		?>
-			
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-
 		<?php fandomonium_post_thumbnail(); ?>
 
-			
 		<section>
-			<?php
-
-			if (function_exists('get_field')) :
-				if (get_field('description')) :
-					?>
-						<p><?php the_field('description'); ?></p>
-					<?php
-				endif;
-			endif;
-
-			$args = array(
-				'post_type' 	 => 'fan-event',
-				'posts_per_page' => -1,
-				'orderby'        => 'meta_value',
-    			'meta_key'       => 'start_time',
-				'order' 		 => 'ASC',
-			);
-
 		
-			$query = new WP_Query( $args );
-			
-				if ( $query -> have_posts() ) {
-					?>
-					<div class="anchor-container day-one">
-						<h2>Day One</h2>
-						<?php  
-							while ( $query -> have_posts() ) {
-								$query -> the_post();
-									if (get_field('day') === 'one') {
-								
-									?>
-									<article class="schedule day-one">
-										<a href="<?php echo get_permalink(); ?>">
-											<h2><?php echo get_the_title(); ?></h2>
-											<?php the_post_thumbnail('thumbnail'); ?>
-										</a>
-										<p>Type: <?php echo strip_tags(get_the_term_list($post->ID, 'fan-event-type', '')); ?></p>
-										<p>Date: <?php $startTime = explode('2022', get_field('start_time')); echo $startTime[0]; ?></p>
-										<p>Start: <?php $startTime = explode('2022', get_field('start_time')); echo $startTime[1]; ?></p>
-										<p>End: <?php the_field('end_time') ?></p>
-									</article>
-									<?php
-								}
-							
-							}
-						wp_reset_postdata();
-						?>
-					</div>
-
-					<div class="anchor-container day-two">
-					<h2>Day Two</h2>
-						<?php  
-							while ( $query -> have_posts() ) {
-								$query -> the_post();
-									if (get_field('day') === 'two') {
-								
-									?>
-									<article class="schedule day-two">
-										<a href="<?php echo get_permalink(); ?>">
-											<h2><?php echo get_the_title(); ?></h2>
-											<?php the_post_thumbnail('thumbnail'); ?>
-										</a>
-										<p><?php echo strip_tags(get_the_term_list($post->ID, 'fan-event-type', '')); ?></p>
-										<p>Date: <?php $startTime = explode('2022', get_field('start_time')); echo $startTime[0]; ?></p>
-										<p>Start: <?php $startTime = explode('2022', get_field('start_time')); echo $startTime[1]; ?></p>
-										<p>End: <?php the_field('end_time') ?></p>
-									</article>
-									<?php
-								}
-							
-							}
-						wp_reset_postdata();
-						?>
-					</div>	
-					
-					<?php
-				
-				}
-			
-			
-			?>
 		</section>
-			<?php
 
+		<?php
 		endwhile; // End of the loop.
 		?>
 
@@ -121,5 +37,4 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
