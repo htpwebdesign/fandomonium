2d91b738d6fb0474891829ff33d3942cde2df489
ckodev <cowens17@my.bcit.ca>
2022-06-28 11:48:29 -0700

    re-write event schedule query day one

--- page-schedule.php
+++ page-schedule.php
@@ -57,50 +57,40 @@ get_header();
 
 		
 			$query = new WP_Query( $args );
-			if ( $query -> have_posts() ) {
-				
 			
+				
 				if ( $query -> have_posts() ) {
-					echo "<div class='anchor-container day-one'>";
-					while ( $query -> have_posts() ) {
-						$query -> the_post();
-						
-						if (get_field('start_time') ===  'August 29, 2022 11:00 am') {
-							echo "</div>";
-							echo "<div class='anchor-container day-two'>";
-						}
-
-						echo "<article class='student-item'>";
-								echo '<a href="'.get_permalink().'">';
-									echo "<h2>".get_the_title()."</h2>";
-									the_post_thumbnail( 'thumbnail' );
-								echo '</a>';
-							the_excerpt();
-
-							echo "<p>";
-								echo strip_tags(get_the_term_list($post->ID, 'fan-event-type', ''));
-							echo "</p>";
-
-							echo "<p>";
-								the_field('start_time');
-							echo "</p>";
-
-							echo "<p>";
-								the_field('end_time');
-							echo "</p>";
-
+					?>
+					<div class="anchor-container day-one">
+						<?php  
+							while ( $query -> have_posts() ) {
+								$query -> the_post();
+									if (get_field('day') === 'one') {
+										echo 'test';
+								
+									?>
+									<article class="schedule day-one">
+										<a href="<?php get_permalink(); ?>">
+											<h2><?php get_the_title(); ?></h2>
+											<?php the_post_thumbnail(); ?>
+										</a>
+										<p><?php strip_tags(get_the_term_list($post->ID, 'fan-event-type', '')); ?></p>
+										<p>Date: <?php $startTime = explode('2022', get_field('start_time')); echo $startTime[0]; ?></p>
+										<p>Start: <?php $startTime = explode('2022', get_field('start_time')); echo $startTime[1]; ?></p>
+										<p>End: <?php the_field('end_time') ?></p>
+									</article>
+									<?php
+								}
 							
-					
-						echo "</article>";
-
-						
-		
-					}
-					wp_reset_postdata();
-					echo "</div>";
+							}
+						wp_reset_postdata();
+						?>
+					</div>
+					<?php
+				
 				}
-			wp_reset_postdata();
-			}
+			
+			
 			?>
 			
 
