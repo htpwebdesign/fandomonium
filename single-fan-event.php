1060737c6f7901a37e76d9cc902fe16551f999b9
ckodev <cowens17@my.bcit.ca>
2022-06-28 14:06:54 -0700

    output event title

--- single-fan-event.php
+++ single-fan-event.php
@@ -16,7 +16,11 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			
+			if ( is_singular() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif;
 
 
 			if (function_exists('get_field')) :
