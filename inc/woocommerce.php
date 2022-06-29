ab1c9ad15f9023c07195a7cc0fe2400187e77a69
Fiona Y <fyeung21@gmail.com>
2022-06-29 16:24:43 -0700

    Move number in Cart and Checkout link to before the end of main tag

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -263,23 +263,19 @@ function add_product_excerpt() {
 // want to change the text to be "Details"
 
 /**
- * Move woocommerce end content wrapper lower on the priority
- */
-remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-add_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 30);
-
-/**
  * Show number of items in Cart
  */
 function show_number_of_cart_items() {
+	echo "<div>";
 	echo "<p>You have ". WC()->cart->get_cart_contents_count() ." item(s) in your Cart</p>";
 }
-add_action('woocommerce_after_main_content', 'show_number_of_cart_items', 10);
+add_action('woocommerce_after_shop_loop', 'show_number_of_cart_items', 20);
 
 /**
  * Checkout button to Cart page
  */
 function checkout_button() {
 	echo "<a class='checkout-btn' href='https://fandomonium.bcitwebdeveloper.ca/cart/'>checkout</a>";
+	echo "</div>";
 }
-add_action('woocommerce_after_main_content', 'checkout_button', 20);
+add_action('woocommerce_after_shop_loop', 'checkout_button', 30);
