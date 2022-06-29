59a9ede969ce686c98f33d02624dd6080bf98179
ckodev <cowens17@my.bcit.ca>
2022-06-29 10:05:07 -0700

    delete unnecessary content

--- template-parts/content-bottom-page-template.php
+++ template-parts/content-bottom-page-template.php
@@ -1,18 +1,6 @@
 <?php
-/**
- * Template part for displaying posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Fandomonium
- */
+
+
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-
-	
-
-
-</article><!-- #post-<?php the_ID(); ?> -->
