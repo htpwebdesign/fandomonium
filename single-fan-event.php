5680f3f2f22acdd7ea194fe853e0e032f8659812
ckodev <cowens17@my.bcit.ca>
2022-06-30 14:54:53 -0700

    remove if (is_singular). its always singular.

--- single-fan-event.php
+++ single-fan-event.php
@@ -16,11 +16,9 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			if ( is_singular() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			endif;
+			
+			the_title( '<h1 class="entry-title">', '</h1>' );
+			
 
 			fandomonium_post_thumbnail();
 
