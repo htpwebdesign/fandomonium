49bdb6f110a7082cc47d13b4c764963cb95db2bf
Fiona Y <fyeung21@gmail.com>
2022-06-30 16:01:15 -0700

    Move article tags past the php conditionals

--- template-parts/page-bottom.php
+++ template-parts/page-bottom.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * Template part for bottom of each page
  *
@@ -8,52 +9,57 @@
  */
 ?>
 <section class="page-bottom-section">
-	<article class="buy-tickets-cta">
-		<?php
-		// Includes CTA from Options subpage 'General'
-		// This piece will not show on archive-product.php
-		if (!is_post_type_archive('product')) {
-			if (function_exists('get_fields')) {
-				if (get_field('page_bottom_cta', 'options')) {
-					$link = get_field('page_bottom_cta', 'options');
-					$link_url = $link['url'];
-					$link_title = $link['title'];
-		?>
+
+	<?php
+	// Includes CTA from Options subpage 'General'
+	// This piece will not show on archive-product.php
+	if (!is_post_type_archive('product')) {
+		if (function_exists('get_fields')) {
+			if (get_field('page_bottom_cta', 'options')) {
+				$link = get_field('page_bottom_cta', 'options');
+				$link_url = $link['url'];
+				$link_title = $link['title'];
+	?>
+				<article class="buy-tickets-cta">
 					<a href="<?php echo esc_url($link_url); ?>">
 						<?php echo esc_html($link_title) ?>
 					</a>
-		<?php
-				}
+				</article>
+	<?php
 			}
 		}
-		?>
-	</article>
-	<article class="feat-vendors">
-		<?php
-		// Include featured vendors
-		// This piece will not show on page-vendors.php
-		if (!is_page(73)) {
-			$args = array(
-				'post_type' 	 => 'fan-vendor',
-				'posts_per_page' => 6,
-				'tax_query' => array(
-					array(
-						'taxonomy' => 'fan-vendor-type',
-						'field'    => 'slug',
-						'terms'    => 'platinum',
-					),
-				)
-			);
+	}
+	?>
+	<?php
+	// Includs section for featured vendors
+	// This piece will not show on page-vendors.php
+	if (!is_page(73)) {
+		$args = array(
+			'post_type' 	 => 'fan-vendor',
+			'posts_per_page' => 6,
+			'tax_query' => array(
+				array(
+					'taxonomy' => 'fan-vendor-type',
+					'field'    => 'slug',
+					'terms'    => 'platinum',
+				),
+			)
+		);
 
-			$query = new WP_Query($args);
-			if ($query->have_posts()) {
+		$query = new WP_Query($args);
+		if ($query->have_posts()) {
+	?>
+			<article class="feat-vendors">
+				<?php
 				while ($query->have_posts()) {
 					$query->the_post();
 					the_title();
 				}
-				wp_reset_postdata();
-			}
+				?>
+			</article>
+	<?php
+			wp_reset_postdata();
 		}
-		?>
-	</article>
+	}
+	?>
 </section>
