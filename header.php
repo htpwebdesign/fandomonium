fb924131d743ea7099c1055dc84751536c27246a
Fiona Y <fyeung21@gmail.com>
2022-06-30 16:07:39 -0700

    Add html tags to fields

--- header.php
+++ header.php
@@ -48,14 +48,15 @@
 		<div>
 			<?php 
 				if (function_exists('get_field')) {
-					if (get_field('start_date_of_convention', 'option')) {
-						the_field('start_date_of_convention', 'option');
-					}
-					if (get_field('end_date_of_convention', 'option')) {
-						the_field('end_date_of_convention', 'option');
+					if (get_field('start_date_of_convention', 'option') && get_field('end_date_of_convention', 'option')) {
+					?>	
+						<p><?php the_field('start_date_of_convention', 'option') . the_field('end_date_of_convention', 'option') ?></p>
+					<?php
 					}
 					if (get_field('location_of_convention', 'option')) {
-						the_field('location_of_convention', 'option');
+					?>
+						<p><?php the_field('location_of_convention', 'option'); ?></p>
+					<?php
 					}
 				}
 			?>
