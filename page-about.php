6ef1125b63d500f6ea5021381c3ec992087a910f
Fiona Y <fyeung21@gmail.com>
2022-06-30 22:29:13 -0700

    Refactor code

--- page-about.php
+++ page-about.php
@@ -27,46 +27,47 @@ get_header();
 			if ( function_exists ( 'get_field' ) ) {
  
 				if ( get_field( 'about_the_organization' ) ) { ?>
-					<h2>About the organization</h2>
-					<p><?php get_field( 'about_the_organization' )?></p>
+					<article>
+						<h2>About the organization</h2>
+						<p><?php the_field( 'about_the_organization' )?></p>
+					</article>
 				<?php
 				}
 				
 				if ( get_field( 'convention_purpose' ) ) { ?>
-					<h2>Convention purpose/statement</h2>
-					<p><?php get_field( 'convention_purpose' )?></p>
+					<article>
+						<h2>Convention purpose/statement</h2>
+						<p><?php the_field( 'convention_purpose' )?></p>
+					</article>
 				<?php
 				}
 				
 		}?>
-		<h2>Location</h2>
+			<article>
+				<h2>Location</h2>
 
-		<?php
-			$my_map = get_field('map');
-			$my_map['address'];
-			do_shortcode('[wpgmza id="1"]');
-		?>
-		<h2>Contact Form</h2>
+				<?php
+					$my_map = get_field('map');
+					$my_map['address'];
+					do_shortcode('[wpgmza id="1"]');
+				?>
+			</article>
+			<article>
+				<h2>Contact Form</h2>
 
-		<?php 
-			do_shortcode('[contact-form-7 id="9" title="Contact form 1"]');
-			
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-			
-		endwhile; // End of the loop.
+				<?php 
+					echo do_shortcode('[contact-form-7 id="9" title="Contact form 1"]');
 
-		// make acf for about page AK 
+				endwhile; // End of the loop.
 
-		?>
+				// make acf for about page AK 
+				?>
+			</article>
 		</section>
-		
+
 		<?php get_template_part('template-parts/page', 'bottom'); ?>
 
 	</main><!-- #main -->
 
 <?php
-
 get_footer();
