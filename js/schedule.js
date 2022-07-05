730fb8724752dd8e569d59902507362eec19fea6
ckodev <cowens17@my.bcit.ca>
2022-07-05 13:45:48 -0700

    conditional for active class on btn's

--- js/schedule.js
+++ js/schedule.js
@@ -1,4 +1,6 @@
-function showDay(evt, day) {
+
+
+function showDay(event, day) {
     let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -9,5 +11,13 @@ function showDay(evt, day) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
     document.getElementById(day).style.display = "block";
-    evt.currentTarget.className += " active";
+    if (event) {
+        event.currentTarget.className += " active";
+    } else {
+        let btn = document.getElementById('btnOne');
+        btn.classList.add('active');
+    }
+    
   }
+  window.addEventListener('load', showDay(event, 'day-one'));
+  
