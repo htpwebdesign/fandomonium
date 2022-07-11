c78cf83e12d9798a9183c53aaabf8a65ca534a80
Erin David <erindavid001@gmail.com>
2022-07-11 12:02:04 -0700

    add styles to accordion

--- js/accordion.js
+++ js/accordion.js
@@ -9,10 +9,10 @@ for (i = 0; i < acc.length; i++) {
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (!this.classList.contains("active")) {
+      panel.classList.toggle('cakes')
     } else {
-      panel.style.display = "block";
+      panel.classList.remove('cakes');
     }
   });
 }
