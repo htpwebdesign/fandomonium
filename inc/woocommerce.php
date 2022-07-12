df069941c8a2c94fb4398e7a7137caae8ab3ed47
Fiona Y <fyeung21@gmail.com>
2022-07-12 14:51:24 -0700

    Style name of ticket, price, and add currency

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -21,7 +21,7 @@ function fandomonium_woocommerce_setup() {
 		'woocommerce',
 		array(
 			'thumbnail_image_width' => 300,
-			'single_image_width'    => 300,
+			'single_image_width'    => 600,
 			'product_grid'          => array(
 				'default_rows'    => 3,
 				'min_rows'        => 1,
@@ -324,6 +324,11 @@ remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_pr
  * Remove Related Products
  */
 remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
+
+/**
+ * Display currency next to price
+ */
+add_action('woocommerce_single_product_summary', 'add_currency', 15);
 
 /**
  * Add number of items in cart and checkout link
